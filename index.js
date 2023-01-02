cc905ff95944a63dea463ee7634a22fc7598e15b
robuGdada <paudelronish@gmail.com>
2023-01-02 19:46:15 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello,Dada ronish');
+});
+
+app.get('/will', function (req, res) {
+    res.send( "Hello World" );
+});
+app.get('/ready', function (req, res) {
+    res.send( " Great!, It works!" );
+});
+app.listen(process.env.PORT || 3000);
+module.exports = app;
